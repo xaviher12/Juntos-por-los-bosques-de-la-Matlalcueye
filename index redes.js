f26d0e4278ea323433059a0e6b03678c630dd053
xaviher12 <86447625+xaviher12@users.noreply.github.com>
2021-06-24 19:12:38 -0500

    Add files via upload

---
+++ index redes.js
@@ -0,0 +1,26 @@
+window.sr = ScrollReveal ();
+
+sr.reveal('.navigation', {
+	duration: 3000,
+	origin: 'bottom'
+});
+
+sr.reveal('.h1', {
+	duration: 3000,
+	origin: 'bottom'
+});
+
+sr.reveal('#pultimate', {
+	duration: 3000,
+	origin: 'bottom'
+});
+
+sr.reveal('.links2', {
+	duration: 3000,
+	origin: 'bottom'
+});
+
+sr.reveal('#yo', {
+	duration: 3000,
+	origin: 'bottom'
+});
---
+++ index.js
@@ -0,0 +1,80 @@
+window.sr = ScrollReveal ();
+
+sr.reveal('.navigation', {
+	duration: 3000,
+	origin: 'bottom'
+});
+
+sr.reveal('.tittle', {
+	duration: 3000,
+	origin: 'bottom'
+});
+
+sr.reveal('.texto', {
+	duration: 3000,
+	origin: 'left'
+});
+sr.reveal('h2', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('h3', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('#bosque1', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('.texto2', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('.texto3', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('#gusano', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('.texto4', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('#panoramas', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('#talar', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('.texto5', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('#Co2', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('.texto6', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('#inyeccion2', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('.ultimo2', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('.li', {
+	duration: 3000,
+	origin: 'right'
+});
+sr.reveal('#yo2', {
+	duration: 3000,
+	origin: 'right'
+});
